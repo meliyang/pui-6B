8959a1d8fbc10bc9debf0cc2589f66058d32a368
Melissa Yang <mwyang@andrew.cmu.edu>
2022-04-02 23:44:52 -0400

    implemented local storage

--- main.js
+++ main.js
@@ -1,68 +1,100 @@
 console.log("hello");
-var count = 1; // TODO: initialize with 0 until items have been added; for now, hardcoded with 1
+var count = 0;
 
 // Counter for items
 var counterDisplayElem = document.getElementById('counter-display');
 var counterMinusElem = document.getElementById('counter-minus');
 var counterPlusElem = document.getElementById('counter-plus');
 // Cart item amount
-var addToCartElem = document.getElementById('addToCart');
+var addToCartElem = document.getElementById('addToCart'); // only on product-detail.html
 var cartCounter = document.getElementById('cart-count');
-// Cart items
-const cart = [];
+
 // Prices
 var cookieTotal = document.getElementById('cookie-total');
-var cartTotal = document.getElementById('total')
+var cartTotal = document.getElementById('total');
+let helpElem = document.getElementById('help-frequency'); // Help on product-detail.html (frequency)
+
 
 function addCount() {
   count++;
   counterDisplayElem.innerHTML = count;
 }
-
 function subtractCount() {
   count--;
   counterDisplayElem.innerHTML = count;
 }
 
 function updateCartCount() {
-  cartCounter.innerHTML = cart.length;
+  let cartTmp = JSON.parse(localStorage.getItem("cart-storage"));
+  if (cartTmp != null) {
+    myCart = cartTmp;
+  }
+  myCart.count = myCart.items.length;
+  console.log("updateCartCount", myCart.items);
+  cartCounter.innerHTML = myCart.count;
+  localStorage.setItem("cart-storage", JSON.stringify(myCart));
 }
-function updateTotal() { //TODO
+function updateTotal() {
+  let cartTmp = JSON.parse(localStorage.getItem("cart-storage"));
+  console.log("in updateTotal() cartTmp:", cartTmp);
+  if (cartTmp === null) {
+    cartTmp = myCart;
+  }
+
   newTotal = 0;
-  // for (let item in MyCart.items) {
-  //   newTotal += item.count * item.cost;
-  // }
-  // MyCart.total = newTotal;
-  // cartTotal = MyCart.total
+  for (let itemInd in cartTmp.items) {
+    let item = cartTmp.items[itemInd];
+    console.log("count and cost:", item.quantity, item.cost);
+    newTotal += parseInt(item.quantity) * parseInt(item.cost);
+  }
+  cartTmp.total = newTotal;
+  try {
+    cartTotal.innerHTML = cartTmp.total;
+  }
+  catch(typeError) {
+    console.log("not on cart.html");
+  }
+  finally{
+    localStorage.setItem("cart-storage", JSON.stringify(cartTmp));
+  }
+
 }
 function addToCart() {
-  newCookie = ChocoCookie(count, "day"); //need to add for frequency
-  cart.push(newCookie);
-  console.log("len", cart.length)
+  let q = counterDisplayElem.innerHTML;
+  let frequencyElem = document.getElementById("frequency");
+  let f = frequencyElem.selectedIndex;
+  newCookie = new ChocoCookie(q, f);
+  myCart.items.push(newCookie);
+  updateTotal();
   updateCartCount();
+  alert("Item added to cart!");
+
+  console.log(myCart, "len of cart", myCart.count);
+  localStorage.setItem("cart-storage", JSON.stringify(myCart));
+  console.log("retrieved:", JSON.parse(localStorage.getItem("cart-storage")));
 }
 
-function ChocoCookie(count, frequency) {
-  this.name = "chocolate-chip";
-  this.count = count;
+function ChocoCookie(quantity, frequency) {
+  this.name = "Chocolate chip cookie";
+  this.quantity = quantity;
   this.frequency = frequency;
   this.cost = 1;
   this.image = "img/cookie.png";
   this.image_alt = "chocolate chip cookie";
 }
 
-function GingerbreadCookie(name, count, frequency, cost) {
-  this.name = "gingerbread";
-  this.count = count;
+function GingerbreadCookie(name, quantity, frequency, cost) {
+  this.name = "Gingerbread cookie";
+  this.quantity = quantity;
   this.frequency = frequency;
   this.cost = 2;
   this.image = "img/gingerbread.png";
   this.image_alt = "gingerbread cookie";
 }
 
-function Biscuit(name, count, frequency, cost) {
-  this.name = "biscuit";
-  this.count = count;
+function Biscuit(name, quantity, frequency, cost) {
+  this.name = "Biscuit";
+  this.quantity = quantity;
   this.frequency = frequency;
   this.cost = 1;
   this.image = "img/biscuit.png";
@@ -70,22 +102,85 @@ function Biscuit(name, count, frequency, cost) {
 }
 
 
-// function MyCart(items, count, total) {
-//   this.items = items; //list of Cookie/Cake instances in shopping cart
-//   this.count = length(items);
-//   this.total = total;
-// }
+function Cart(items, total) {
+  this.items = items; //list of Cookie/Cake instances in shopping cart
+  this.count = items.length;
+  this.total = total;
+}
 
-function remove() {
-  var element = document.getElementById("cookie-1"); // TODO: remove current item, currently hard coded to delet cookie-1
-  element.remove();
+function removeFromCart() {
+
+  if (confirm("Are you sure you want to delete this item?")) {
+    let cartTmp = JSON.parse(localStorage.getItem("cart-storage"));
+    console.log(cartTmp);
+    cartTmp.items.shift();
+    cartTmp.count = cartTmp.items.length;
+    localStorage.setItem("cart-storage", JSON.stringify(cartTmp));
+    updateTotal();
+    loadCart();
+    updateCartCount();
+
+  }
+
+}
+
+function loadCart() {
+  let cartTmp = JSON.parse(localStorage.getItem("cart-storage"));
+  console.log("loadCart() cartTmp:", cartTmp);
+  // empty cart
+  if (cartTmp === null || cartTmp.count === 0) {
+
+    console.log("no cart");
+
+    document.getElementById("item-1-image").src = "img/night.png"
+    document.getElementById("item-1-name").innerHTML = "Add an item to the cart!"
+
+    document.getElementsByClassName("counter")[0].style.visibility = "hidden";
+    document.getElementById("slash").style.visibility = "hidden";
+    document.getElementById("frequency").style.visibility = "hidden";
+    document.getElementsByClassName("item-price")[0].style.visibility = "hidden";
+    document.getElementsByClassName("button-remove")[0].style.visibility = "hidden";
+  }
+  // not empty cart
+  else {
+    console.log("cart is not empty:", cartTmp);
+    let items = cartTmp.items;
+    // TODO: add multiple items to cart, need to loop through items
+    let item1 = items[0];
+    console.log("item1:", item1);
+    console.log(item1.cost, item1.quantity);
+
+    document.getElementById("item-1-image").src = item1.image;
+    document.getElementById("item-1-image").alt = item1.image_alt;
+    document.getElementById("item-1-name").innerHTML = item1.name;
+    document.getElementById("counter-display").innerHTML = item1.quantity;
+    document.getElementById("frequency").selectedIndex = item1.frequency;
+    document.getElementById("item-1-total").innerHTML = parseInt(item1.cost) * parseInt(item1.quantity)
+    updateTotal();
+    updateCartCount();
+  }
+}
+// all.html
+myCart = new Cart([], 0);
+
+
+
+
+// product-detail.html --
+
+
+function helpFreq() {
+  console.log("clicked help button");
+  alert("Frequency of subscription --\n\nday: shipped every day at 8 am\nweek: shipped every week on Monday\nmonth: shipped every month on the first day of each month");
 }
 
 
-counterPlusElem.addEventListener("click", addCount);
-counterMinusElem.addEventListener("click", subtractCount);
+// cart.html --
+try {
+  loadCart();
+}
 
-addToCartElem.addEventListener("click", addToCart);
-
-
+catch (typeError) {
+  console.log("not on cart.html");
+}
 
